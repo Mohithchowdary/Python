eb65e4d28d9d2aeb7dce196f8f745e72afd40237
Anónimo <35984716+Mohithchowdary@users.noreply.github.com>
2021-08-21 08:34:19 +0530

    Update Lab Program - 01.py

--- Lab Program - 01.py
+++ Lab Program - 01.py
@@ -1,7 +1,7 @@
 """
 1. Write programs to implement the following operations:
-a. To elaborate variables and their data types such as int, float, boolean, string, list, set, dictionary and tuples; swap two numbers.
-b. To perform mathematical operations such as addition, subtraction, multiplication, division, modulo, and power; also explore the operator precedence.
+   a. To elaborate variables and their data types such as int, float, boolean, string, list, set, dictionary and tuples; swap two numbers.
+   b. To perform mathematical operations such as addition, subtraction, multiplication, division, modulo, and power; also explore the operator precedence.
 """
 
 import os
