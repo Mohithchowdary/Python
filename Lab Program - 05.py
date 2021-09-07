89a52fda5f35235ce36c27719e9d6579fca5d711
Mohith Chowdary <35984716+Mohithchowdary@users.noreply.github.com>
2021-09-07 16:04:41 +0530

    Update Lab Program - 05.py

--- Lab Program - 05.py
+++ Lab Program - 05.py
@@ -1,4 +1,5 @@
-'''Write Python program:
+'''
+Write Python program:
     a. Implement python script to show the usage of various operators available in python
     b. Write a program to calculate overtime pay of 10 employees. Overtime is paid at the rate of Rs. 12.00 per hour for every hour worked above 40 hours. Assume that employee do not work for fractional part of an hour.
 '''
